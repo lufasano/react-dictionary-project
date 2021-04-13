7d5f4ef9c7d75a179675430bfeb4a4e10de9b416
lufasano <66181095+lufasano@users.noreply.github.com>
2021-04-13 18:54:11 -0300

    Correcting errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
     </div>
   );
 }
-
-export default App;
