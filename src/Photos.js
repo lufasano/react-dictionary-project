b9c7853d133c0d299755d4d3569dd298c282ec13
lufasano <66181095+lufasano@users.noreply.github.com>
2021-04-21 15:08:06 -0300

    adding alt

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,11 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 {" "}
-                <img src={photo.src.landscape} className="img-fluid" />
+                <img
+                  src={photo.src.landscape}
+                  className="img-fluid"
+                  alt="photo"
+                />
               </div>
             );
           })}
